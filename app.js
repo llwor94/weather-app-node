bcd75ffd6622faf23dee82116701343ff09df261
Lauren <llwor94@gmail.com>
2018-10-24 21:51:38 -0700

    error handling and user input

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const request = require('request');
 const yargs = require('yargs');
 
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Address to fetch weather for',
+			string: true,
+		},
+	})
+	.help()
+	.alias('help', 'h').argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+
 request(
 	{
-		url:
-			'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBvvJ2RPSb2sua8pR-yXUf8sRO1BhlBjeE&address=1301%20lombard%20street%20philadelphia',
+		url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBvvJ2RPSb2sua8pR-yXUf8sRO1BhlBjeE&address=${encodedAddress}`,
 		json: true,
 	},
 	(error, response, body) => {
-		console.log(`Address: ${body.results[0].formatted_address}`);
-		console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-		console.log(`Long: ${body.results[0].geometry.location.lng}`);
+		if (error) {
+			console.log('Unable to connect to Google servers.');
+		} else if (body.status === 'ZERO_RESULTS') {
+			console.log('Unable to find address.');
+		} else if (body.status === 'OK') {
+			console.log(`Address: ${body.results[0].formatted_address}`);
+			console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+			console.log(`Long: ${body.results[0].geometry.location.lng}`);
+		}
 	},
 );
